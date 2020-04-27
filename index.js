35edf48e0f2380048fc3f1991e7a0a5fbda30bd8
Evgeniy Smirniy <evgeniy.s@zazmic.com>
2020-04-27 14:34:29 +0300

    SUPER-1 One more check

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ console.log("Thats fucking test");
 console.log("That's fucking another test");
 console.log("Thats fucking another commit");
 console.log("That's another");
+console.log("Thats one more test");
